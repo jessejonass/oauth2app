2fcdcdae784dd7d71e73638d25651a7f028e9da0
jessejonas13@gmail.com <jessejonas13@gmail.com>
2021-11-12 08:50:50 -0300

    feat: data displayed on profile page

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Feather } from '@expo/vector-icons';
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
 
-import { ProfileHeader } from '../../components/ProfileHeader';
-import { Avatar } from '../../components/Avatar';
-import { Button } from '../../components/Button';
+import { ProfileHeader } from "../../components/ProfileHeader";
+import { Avatar } from "../../components/Avatar";
+import { Button } from "../../components/Button";
 
-import { styles } from './styles';
-import { theme } from '../../styles/theme';
+import { styles } from "./styles";
+import { theme } from "../../styles/theme";
+
+type Params = {
+  token: string;
+};
+
+type Profile = {
+  name: string;
+  family_name: string;
+  given_name: string;
+  locale: string;
+  email: string;
+  picture: string;
+};
 
 export function Profile() {
+  const [profile, setProfile] = useState({} as Profile);
+
+  const route = useRoute();
   const navigation = useNavigation();
 
+  const { token } = route.params as Params;
+
   async function handleLogout() {
-    navigation.navigate('SignIn');
+    navigation.navigate("SignIn");
   }
+
+  async function loadProfile() {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${token}`
+    );
+    const userInfo = await response.json();
+    setProfile(userInfo);
+  }
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -23,69 +53,39 @@ export function Profile() {
 
       <View style={styles.content}>
         <View style={styles.profile}>
-          <Avatar
-            source={{ uri: 'https://github.com/rodrigorgtic.png' }}
-          />
+          <Avatar source={{ uri: profile.picture }} />
 
-          <Text style={styles.name}>
-            Rodrigo Gonçalves
-          </Text>
+          <Text style={styles.name}>{profile.name}</Text>
 
           <View style={styles.email}>
             <Feather name="mail" color={theme.colors.secondary} size={18} />
-            <Text style={styles.emailText}>
-              rodrigo.goncalves@rocketseat.team
-            </Text>
+            <Text style={styles.emailText}>{profile.email}</Text>
           </View>
         </View>
 
         <View style={styles.about}>
           <View style={styles.info}>
-            <Feather
-              name="user"
-              size={34}
-              color={theme.colors.note}
-            />
-            <Text style={styles.label}>
-              Nome
-            </Text>
-            <Text style={styles.text}>
-              Rodrigo
-            </Text>
+            <Feather name="user" size={34} color={theme.colors.note} />
+            <Text style={styles.label}>Nome</Text>
+            <Text style={styles.text}>{profile.given_name}</Text>
           </View>
 
           <View style={styles.info}>
-            <Feather
-              name="heart"
-              size={34}
-              color={theme.colors.note}
-            />
-            <Text style={styles.label}>
-              Sobrenome
-            </Text>
-            <Text style={styles.text}>
-              Gonçalves
-            </Text>
+            <Feather name="heart" size={34} color={theme.colors.note} />
+            <Text style={styles.label}>Sobrenome</Text>
+            <Text style={styles.text}>{profile.family_name}</Text>
           </View>
         </View>
 
         <View style={styles.locale}>
-          <Feather
-            name="map-pin"
-            size={18}
-            color={theme.colors.note}
-          />
+          <Feather name="map-pin" size={18} color={theme.colors.note} />
 
           <Text style={styles.localeText}>
-            Localidade do perfil do usuário: pt-BR
+            Localidade do perfil do usuário: {profile.locale}
           </Text>
         </View>
 
-        <Button
-          title="Desconectar"
-          icon="power"
-          onPress={handleLogout}
-        />
+        <Button title="Desconectar" icon="power" onPress={handleLogout} />
       </View>
     </View>
   );
