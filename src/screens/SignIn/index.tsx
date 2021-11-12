297b1d6a97d08c59d5800bee5d17c6431bbfc14a
jessejonas13@gmail.com <jessejonas13@gmail.com>
2021-11-12 08:31:26 -0300

    feat: added successful login flow and redirection

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -27,10 +27,13 @@ export function SignIn() {
 
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-    const response = await AuthSession.startAsync({ authUrl });
-    console.log(response);
+    const { type, params } = (await AuthSession.startAsync({
+      authUrl,
+    })) as AuthResponse;
 
-    navigation.navigate("Profile");
+    if (type === "success") {
+      navigation.navigate("Profile", { token: params.access_token });
+    }
   }
 
   return (
